227978f26459c8dc522c6e17e44bd3a8e0173f41
Шурмин Евгений <eshurmin@gmail.com>
2012-04-04 11:03:25 +0400

    Support for Redmine 1.3.2

--- init.rb
+++ init.rb
@@ -4,7 +4,9 @@ Redmine::Plugin.register :redmine_active_issues do
   name 'Redmine Active Issues plugin'
   author 'Evgeniy Shurmin'
   description 'This is a plugin add tab to each project to list active issues'
-  version '0.0.1'
+  version '0.0.2'
+  url 'https://github.com/jpascal/redmine_active_issues'
+  author_url 'https://github.com/jpascal'
   settings :default => {
       "status_ids" => []
   }, :partial => 'settings/activeissues_settings'
